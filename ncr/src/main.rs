ac9816b2434aad3b0af4e0686bf9ea081555e257
maetin0324 <maetin0324@gmail.com>
2024-02-02 10:46:19 +0900

    本の内容じゃないので別リポジトリに移した

--- ncr/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    if let Err(e) = ncr::get_args().and_then(ncr::run) {
-        eprintln!("Application error: {}", e);
-        std::process::exit(1);
-    }
-}
--- ncr/tests/cli.rs
+++
@@ -1,6 +0,0 @@
-use assert_cmd::Command;
-use predicates::prelude::*;
-use rand::{distributions::Alphanumeric, Rng};
-use std::fs;
-
-type TestResult = Result<(), Box<dyn std::error::Error>>;
